355674992c0658afeb833cb29f80ffcbb10ec34f
Alexis <63883086+Alaixs@users.noreply.github.com>
2023-05-03 23:47:40 +0200

    Rewrite and fix some bug

--- main-rpiOSlite.py
+++ main-rpiOSlite.py
@@ -2,6 +2,8 @@ import time
 import os
 import json
 import requests
+import pytz
+import datetime
 from dotenv import load_dotenv
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -10,51 +12,50 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from pyvirtualdisplay import Display
 
+
 ##################
 # INITIALISATION #
 ##################
 
-#Chemin de ChromeDriver
+# Chemin de ChromeDriver
 chrome_path = "/usr/lib/chromium-browser/chromedriver"
-
-#Initialisation de Selenium WebDriver headless
-service = Service(chrome_path)
-display = Display(visible=0, size=(800, 600))
-display.start()
-driver = webdriver.Chrome(service=service)
+tz = pytz.timezone('Europe/Paris')
+nb_request = 0
 
 load_dotenv()
-username_ent = os.getenv('USERNAME_ENT')
-password_ent = os.getenv('PASSWORD_ENT')
-wehbhook_url = os.getenv('WEBHOOK_URL')
+username_ent = os.getenv("USERNAME_ENT")
+password_ent = os.getenv("PASSWORD_ENT")
+wehbhook_url = os.getenv("WEBHOOK_URL")
 
 payload_discord = {
-  "embeds": [
-    {
-      "title": "Nouvelle note disponible !",
-      "description": "",
-      "color": 43287
-    }
-  ],
-  "attachments": []
+    "embeds": [
+        {"title": "Nouvelle note disponible !", "description": "", "color": 43287}
+    ],
+    "attachments": [],
 }
 
 
-    ###########################
-	# 	  Payload Discord 	  #
-	###########################
+###########################
+# 	  Payload Discord 	  #
+###########################
+
 
 def send_discord_message(get_is_year):
     if get_is_year:
-        payload_discord["embeds"][0]["description"] = "👨‍🎓 🟢 Actuellement tu as ton année !"
+        payload_discord["embeds"][0][
+            "description"
+        ] = "👨‍🎓 🟢 Actuellement tu as ton année !"
     else:
-        payload_discord["embeds"][0]["description"] = "👨‍🎓 🔴 Actuellement tu n'as pas ton année !"
+        payload_discord["embeds"][0][
+            "description"
+        ] = "👨‍🎓 🔴 Actuellement tu n'as pas ton année !"
     requests.post(wehbhook_url, json=payload_discord)
+
 
 def get_all_cookies(driver):
     ###########################
-	# 	Cookie de l'ENT lr	  #
-	###########################
+    # 	Cookie de l'ENT lr	  #
+    ###########################
     url_general_id = "https://authentification.univ-lr.fr/cas/login"
     driver.get(url_general_id)
 
@@ -71,19 +72,24 @@ def get_all_cookies(driver):
     password_input.submit()
 
     ###########################
-	# 	Cookie de notes IUT   #
-	###########################
+    # 	Cookie de notes IUT   #
+    ###########################
     url_notes_id = "https://notes.iut-larochelle.fr/"
     driver.get(url_notes_id)
 
     wait = WebDriverWait(driver, 10)
-    wait.until(EC.invisibility_of_element_located(
-        (By.XPATH, '//body[@class="hideAbsences etudiant"]/div[@class="auth" and @style="opacity: 0; pointer-events: none;"]')))
+    wait.until(
+        EC.invisibility_of_element_located(
+            (
+                By.XPATH,
+                '//body[@class="hideAbsences etudiant"]/div[@class="auth" and @style="opacity: 0; pointer-events: none;"]',
+            )
+        )
+    )
 
-
-    ###########################
-	# 	Récupérer les notes   #
-	###########################
+###########################
+# 	Récupérer les notes   #
+###########################
 def get_ue_averages(driver, url, ue_list):
     driver.get(url)
     texte = driver.find_element(By.TAG_NAME, "pre").text
@@ -93,7 +99,15 @@ def get_ue_averages(driver, url, ue_list):
         driver.get(url)
     content = driver.find_element(By.TAG_NAME, "pre").text
     parsed_json = json.loads(content)
-    ue_averages = [parsed_json["relevé"]["ues"][ue]["moyenne"]["value"] for ue in ue_list]
+    ue_averages = [
+        parsed_json["relevé"]["ues"][ue]["moyenne"]["value"] for ue in ue_list
+    ]
+    with open('last_request_time.txt', 'w', encoding='utf-8') as f:
+        f.write(str(datetime.datetime.now(tz)))
+    global nb_request
+    nb_request += 1
+    with open('nb_request.txt', 'w', encoding='utf-8') as f:
+        f.write(str(nb_request))
     return ue_averages
 
 
@@ -104,10 +118,12 @@ ue_list_semestre2 = ["UE 2.1", "UE 2.2", "UE 2.3", "UE 2.4", "UE 2.5", "UE 2.6"]
 ue_list_semestre1 = ["UE 1.1", "UE 1.2", "UE 1.3", "UE 1.4", "UE 1.5", "UE 1.6"]
 
 
-    ###########################
-	# Check si année validée  #
-	###########################
+###########################
+# Check si année validée  #
+###########################
 old_total = 0
+
+
 def check_note():
     count_below_10 = 0
     for ue_nb in range(0, 6):
@@ -117,24 +133,44 @@ def check_note():
         if res < 10:
             count_below_10 += 1
         if res < 8 or count_below_10 > 2:
-            print("UE", ue_nb+1, "average:", res, "pas validé")
+            print("UE", ue_nb + 1, "average:", res, "pas validé")
             send_discord_message(False)
             return
     send_discord_message(True)
     print("Semestre 1 et 2 validés")
 
-  ###########################
-	#    Boucle principale    #
-	###########################
+
+###########################
+#    Boucle principale    #
+###########################
 
 while True:
-    ue_averages_semestre2 = get_ue_averages(driver, url_semestre2, ue_list_semestre2)
-    ue_averages_semestre1 = get_ue_averages(driver, url_semestre1, ue_list_semestre1)
-    pre = WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.TAG_NAME, "pre")))
-    for i in range(0, 6):
-        actual_total = float(ue_averages_semestre2[i]) + float(ue_averages_semestre1[i])
-    if actual_total != old_total:
-        check_note()
-        old_total = actual_total
-    time.sleep(300)
+    try:
+        # Initialisation de Selenium WebDriver headless
+        service = Service(chrome_path)
+        display = Display(visible=0, size=(800, 600))
+        display.start()
+        driver = webdriver.Chrome(service=service)
+        ue_averages_semestre2 = get_ue_averages(
+            driver, url_semestre2, ue_list_semestre2
+        )
+        ue_averages_semestre1 = get_ue_averages(
+            driver, url_semestre1, ue_list_semestre1
+        )
+        pre = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.TAG_NAME, "pre"))
+        )
+        for i in range(0, 6):
+            actual_total = float(ue_averages_semestre2[i]) + float(
+                ue_averages_semestre1[i]
+            )
+        if actual_total != old_total:
+            check_note()
+            old_total = actual_total
+        print("refresh")
+        print(old_total, actual_total)
+        time.sleep(300)
+    except Exception as e:
+        print("Exception occurred:", e)
+    finally:
+        driver.quit()
