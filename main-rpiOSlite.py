fc36084630d4f37da2d11d998065c4f217ea6e92
alexis piekarz <alexis.piekarz@etudiant.univ-lr.fr>
2023-06-14 18:59:40 +0200

    Improve stability
    Add better control, and better reliability when bug appear

--- main-rpiOSlite.py
+++ main-rpiOSlite.py
@@ -1,8 +1,8 @@
-import time
 import os
 import json
 import requests
 import pytz
+import time
 import datetime
 from dotenv import load_dotenv
 from selenium import webdriver
@@ -10,31 +10,16 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from pyvirtualdisplay import Display
+from selenium.common.exceptions import NoSuchWindowException
+from selenium.common.exceptions import NoSuchElementException
 
-
-##################
-# INITIALISATION #
-##################
-
-# Chemin de ChromeDriver
-options = webdriver.ChromeOptions()
-options.add_argument("--headless")
-options.add_argument("disable-infobars"); # disabling infobars
-options.add_argument("--disable-extensions"); # disabling extensions
-options.add_argument("--disable-gpu"); # applicable to windows os only
-options.add_argument("--disable-dev-shm-usage"); # overcome limited resource problems
-options.add_argument("--no-sandbox"); # Bypass OS security model
-chrome_path = "/usr/lib/chromium-browser/chromedriver"
-service = Service(chrome_path)
-driver = webdriver.Chrome(service=service, options=options)
-tz = pytz.timezone('Europe/Paris')
-nb_request = 0
 
 load_dotenv()
 username_ent = os.getenv("USERNAME_ENT")
 password_ent = os.getenv("PASSWORD_ENT")
 wehbhook_url = os.getenv("WEBHOOK_URL")
+nb_request = 0
+tz = pytz.timezone('Europe/Paris')
 
 payload_discord = {
     "embeds": [
@@ -42,12 +27,6 @@ payload_discord = {
     ],
     "attachments": [],
 }
-
-
-###########################
-#         Payload Discord         #
-###########################
-
 
 def send_discord_message(get_is_year):
     print("send discord message")
@@ -61,10 +40,9 @@ def send_discord_message(get_is_year):
         ] = "👨‍🎓 🔴 Actuellement tu n'as pas ton année !"
     requests.post(wehbhook_url, json=payload_discord)
 
-
 def get_all_cookies(driver):
     ###########################
-    #   Cookie de l'ENT lr        #
+    #   Cookie de l'ENT lr    #
     ###########################
     url_general_id = "https://authentification.univ-lr.fr/cas/login"
     driver.get(url_general_id)
@@ -97,9 +75,6 @@ def get_all_cookies(driver):
         )
     )
 
-###########################
-#       Récupérer les notes   #
-###########################
 def get_ue_averages(driver, url, ue_list):
     driver.get(url)
     texte = driver.find_element(By.TAG_NAME, "pre").text
@@ -109,32 +84,26 @@ def get_ue_averages(driver, url, ue_list):
         driver.get(url)
     content = driver.find_element(By.TAG_NAME, "pre").text
     parsed_json = json.loads(content)
-    ue_averages = [
-        parsed_json["relevé"]["ues"][ue]["moyenne"]["value"] for ue in ue_list
-    ]
-    with open('last_request_time.txt', 'w', encoding='utf-8') as f:
+    ue_averages = [parsed_json["relevé"]["ues"][ue]["moyenne"]["value"] for ue in ue_list]
+    save_request_checked()
+    return ue_averages
+
+def save_request_try():
+    with open('last_request_try.txt', 'w', encoding='utf-8') as f:
         f.write(str(datetime.datetime.now(tz)))
-    global nb_request
+
+def save_request_checked():
+    with open('last_request_checked.txt', 'w', encoding='utf-8') as f:
+        f.write(str(datetime.datetime.now(tz)))
+
+def increment_request():
+    with open('nb_request.txt', 'r', encoding='utf-8') as f:
+        nb_request = int(f.read())
     nb_request += 1
     with open('nb_request.txt', 'w', encoding='utf-8') as f:
         f.write(str(nb_request))
-    return ue_averages
 
-
-url_semestre2 = "https://notes.iut-larochelle.fr/services/data.php?q=relev%C3%A9Etudiant&semestre=401"
-url_semestre1 = "https://notes.iut-larochelle.fr/services/data.php?q=relev%C3%A9Etudiant&semestre=357"
-
-ue_list_semestre2 = ["UE 2.1", "UE 2.2", "UE 2.3", "UE 2.4", "UE 2.5", "UE 2.6"]
-ue_list_semestre1 = ["UE 1.1", "UE 1.2", "UE 1.3", "UE 1.4", "UE 1.5", "UE 1.6"]
-
-
-###########################
-# Check si année validée  #
-###########################
-old_total = 0
-
-
-def check_note():
+def check_note(ue_averages_semestre1, ue_averages_semestre2):
     count_below_10 = 0
     for ue_nb in range(0, 6):
         res = float(ue_averages_semestre1[ue_nb]) + float(ue_averages_semestre2[ue_nb])
@@ -149,41 +118,51 @@ def check_note():
     send_discord_message(True)
     print("Semestre 1 et 2 validés")
 
+def main():
+    chrome_path = "/usr/lib/chromium-browser/chromedriver"
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless")
+    options.add_argument("disable-infobars")
+    options.add_argument("--disable-extensions")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--no-sandbox")
+    service = Service(chrome_path)
+    driver = webdriver.Chrome(service=service, options=options)
 
-###########################
-#    Boucle principale    #
-###########################
+    url_semestre2 = "https://notes.iut-larochelle.fr/services/data.php?q=relev%C3%A9Etudiant&semestre=401"
+    url_semestre1 = "https://notes.iut-larochelle.fr/services/data.php?q=relev%C3%A9Etudiant&semestre=357"
 
-while True:
-    try:
-        # Initialisation de Selenium WebDriver headless
-        display = Display(visible=0, size=(800, 600))
-        display.start()
-        driver = webdriver.Chrome(service=service, options=options)
-        ue_averages_semestre2 = get_ue_averages(
-            driver, url_semestre2, ue_list_semestre2
-        )
-        ue_averages_semestre1 = get_ue_averages(
-            driver, url_semestre1, ue_list_semestre1
-        )
-        pre = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.TAG_NAME, "pre"))
-        )
-        for i in range(0, 6):
-            actual_total = float(ue_averages_semestre2[i]) + float(
-                ue_averages_semestre1[i]
-            )
-        with open('last_note.txt', 'r', encoding='utf-8') as f:
-            last_note = f.read()
-        if actual_total != old_total and last_note != str(actual_total):
-            check_note()
-            old_total = actual_total
-            with open('last_note.txt', 'w', encoding='utf-8') as f:
-                f.write(str(actual_total))
-        print("refresh")
-        print(old_total, actual_total)
-        time.sleep(300)
-    except Exception as e:
-        print("Exception occurred:", e)
-    finally:
-        driver.quit()
+    ue_list_semestre2 = ["UE 2.1", "UE 2.2", "UE 2.3", "UE 2.4", "UE 2.5", "UE 2.6"]
+    ue_list_semestre1 = ["UE 1.1", "UE 1.2", "UE 1.3", "UE 1.4", "UE 1.5", "UE 1.6"]
+
+    old_total = 0
+
+    while True:
+        save_request_try()
+        try:
+            ue_averages_semestre2 = get_ue_averages(driver, url_semestre2, ue_list_semestre2)
+            ue_averages_semestre1 = get_ue_averages(driver, url_semestre1, ue_list_semestre1)
+            actual_total = sum(float(avg) for avg in ue_averages_semestre2 + ue_averages_semestre1)
+
+            with open('last_note.txt', 'r', encoding='utf-8') as f:
+                last_note = f.read()
+                save_request_try()
+            if actual_total != old_total and last_note != str(actual_total):
+                check_note(ue_averages_semestre1, ue_averages_semestre2)
+                old_total = actual_total
+                with open('last_note.txt', 'w', encoding='utf-8') as f:
+                    f.write(str(actual_total))
+
+            print("refresh")
+            print(old_total, actual_total)
+
+            time.sleep(300)
+
+        except NoSuchWindowException:
+            print("Window closed. Reopening...")
+            driver.quit()
+            driver = webdriver.Chrome(service=service, options=options)
+
+if __name__ == "__main__":
+    main()
