8f69464dd27fd59c604b4c86ee75163532068252
apiekarz <63883086+Alaixs@users.noreply.github.com>
2023-06-19 21:53:07 +0200

    add some functions + opti

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use reqwest;
+use reqwest::{Response, Client};
 use select::document::Document;
 use select::predicate::Name;
 use std::env;
@@ -6,58 +6,77 @@ use dotenv::dotenv;
 use serde_json::Value;
 use anyhow::{Result, Context};
 
+const LOGIN_URL: &str = "https://authentification.univ-lr.fr/cas/login?service=https://notes.iut-larochelle.fr/services/doAuth.php?href=https://notes.iut-larochelle.fr/";
+
+
+async fn get_cookies(client: &Client) -> Result<()> {
+    let pre_auth: String = client.get(LOGIN_URL)
+    .send()
+    .await?
+    .text()
+    .await?;
+
+    let username: String = env::var("USERNAME_ENT").context("Le nom d'utilisateur n'est pas défini dans le fichier .env")?;
+    let password: String = env::var("PASSWORD_ENT").context("Le mot de passe n'est pas défini dans le fichier .env")?;
+
+    // Utiliser la bibliothèque select pour extraire la valeur d'exécution
+    let document: Document = Document::from(pre_auth.as_str());
+    let exec_value: &str = document
+        .find(Name("input"))
+        .filter(|n| n.attr("name").unwrap_or("") == "execution")
+        .next()
+        .and_then(|n: select::node::Node<'_>| n.attr("value"))
+        .context("Valeur d'exécution introuvable")?;
+
+    // Données du formulaire
+
+    let form_data: [(&str, &str); 5] = [
+        ("username", username.as_str()),
+        ("password", password.as_str()),
+        ("execution", exec_value),
+        ("_eventId", "submit"),
+        ("geolocation", ""),
+    ];
+
+    // Effectuer une requête POST pour se connecter
+    let auth: Response = client.post(LOGIN_URL)
+        .form(&form_data)
+        .send()
+        .await?;
+    auth.error_for_status_ref().context("Erreur lors de la connexion")?;
+
+    Ok(())
+}
+
+
+
+async fn get_ues(client: &Client) -> Result<()> {
+
+
+}
+
+
 #[tokio::main]
 async fn main() -> Result<()> {
     // Charger les variables d'environnement à partir du fichier .env
     dotenv().ok();
 
     // Récupérer le nom d'utilisateur et le mot de passe à partir des variables d'environnement
-    let username = "xxx";
-    let password = "xxx";
 
-    let login_url = "https://authentification.univ-lr.fr/cas/login?service=https://notes.iut-larochelle.fr/services/doAuth.php?href=https://notes.iut-larochelle.fr/";
+    let client: Client = Client::builder().cookie_store(true).build()?;
 
-    let client = reqwest::Client::builder().cookie_store(true).build()?;
 
+    get_cookies(&client).await?;
     // Effectuer une requête GET pour récupérer la page de connexion
-    let pre_auth = client.get(login_url)
-    .send()
-    .await?
-    .text()
-    .await?;
-
-    // Utiliser la bibliothèque select pour extraire la valeur d'exécution
-    let document = Document::from(pre_auth.as_str());
-    let exec_value = document
-        .find(Name("input"))
-        .filter(|n| n.attr("name").unwrap_or("") == "execution")
-        .next()
-        .and_then(|n| n.attr("value"))
-        .context("Valeur d'exécution introuvable")?;
-
-    // Données du formulaire
-    let form_data = [
-        ("username", username.to_string()),
-        ("password", password.to_string()),
-        ("execution", exec_value.to_string()),
-        ("_eventId", "submit".to_string()),
-        ("geolocation", "".to_string()),
-    ];
-
-    // Effectuer une requête POST pour se connecter
-    let auth = client.post(login_url)
-        .form(&form_data)
-        .send()
-        .await?;
-    auth.error_for_status_ref().context("Erreur lors de la connexion")?;
+    
 
     // Effectuer une requête GET pour récupérer les données
-    let r = client.get("https://notes.iut-larochelle.fr/services/data.php?q=dataPremièreConnexion")
+    let r: Response = client.get("https://notes.iut-larochelle.fr/services/data.php?q=dataPremièreConnexion")
         .send()
         .await?;
     r.error_for_status_ref().context("Erreur lors de la récupération des données")?;
 
-    let text_response = r.text().await?;
+    let text_response: String = r.text().await?;
     let json_response: Value = serde_json::from_str(&text_response)?;
 
     println!("{:?}", json_response);
